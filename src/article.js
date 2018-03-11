a4bf6053862cd5344edd091810c1dec98cbfaae3
takashabe <takashabe.public@gmail.com>
2018-03-12 00:18:28 +0900

    Add articles component

--- src/article.js
+++ src/article.js
@@ -1,31 +1,51 @@
 import React from 'react';
 import axios from 'axios';
 import 'babel-polyfill';
+import List, {ListItem, ListItemIcon, ListItemText} from 'material-ui/List';
 
 export default class Article extends React.Component {
   constructor() {
     super();
     this.state = {
-      article: '',
+      articles: null,
     };
 
-    this.getEntry = this.getEntry.bind(this);
+    this.getTitles = this.getTitles.bind(this);
   }
 
   componentWillMount() {
-    this.getEntry();
+    let start = this.props.start;
+    if (start === undefined) start = 0;
+    this.getTitles(start);
   }
 
-  getEntry() {
-    axios.get('/api/entry/1').then(res => {
+  async getTitles(start) {
+    const len = 10;
+    axios.get(`/api/titles/${start}/${len}`).then(res => {
       this.setState({
-        article: res.data,
+        articles: res.data.data,
       });
-      console.log(this.state);
     });
   }
 
   render() {
-    return <div>{this.state.article}</div>;
+    let rows;
+    if (this.state.articles === null)
+      rows = (
+        <ListItem>
+          <ListItemText primary="Not found articles" />
+        </ListItem>
+      );
+    else
+      rows = this.state.articles.map(n => {
+        return (
+          // TODO(takashabe): link to detail article path with SPA router
+          <ListItem key={n.id} component="a" href="#list">
+            <ListItemText primary={n.title} />
+          </ListItem>
+        );
+      });
+
+    return <List>{rows}</List>;
   }
 }
